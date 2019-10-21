e0ed703d86a55c2b983da67d715b820ffe022fa7
hatter222 <56596758+hatter222@users.noreply.github.com>
2019-10-21 13:27:51 +0200

    Update_arcmethond_twocircles

--- Exercise1/Basic/Basic2.js
+++ Exercise1/Basic/Basic2.js
@@ -9,10 +9,20 @@ context.arc(60,60, 50, 0, 2 * Math.PI);
 context.fillStyle = 'rgb(0,255,0)'
 context.fill();
 
+
+
 context.beginPath();
-context.arc(140,140, 50, 0, 2 * Math.PI);
+context.arc(140,140, 55, 0, 2 * Math.PI);
+context.fillStyle = 'rgb(0,127,0)'
+context.fill();
+
+
+context.beginPath();
+context.arc(140,140, 45, 0, 2 * Math.PI);
 context.fillStyle = 'rgb(0,255,0)'
 context.fill();
+
+
 
 
 
